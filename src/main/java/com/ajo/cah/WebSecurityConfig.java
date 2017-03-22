327d4bfa8568cbd091243d53821ba4adb9c50789
Andyman <ajo0131@gmail.com>
2017-03-22 02:14:02 -0400

    Use JDBC authentication

--- src/main/java/com/ajo/cah/WebSecurityConfig.java
+++ src/main/java/com/ajo/cah/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.ajo.cah;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
@@ -16,50 +23,51 @@ import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
+  
   public static final String LOGIN_PATH = "/login";
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home", "/resources/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage(LOGIN_PATH)
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll()
-            .and().csrf()
-              .csrfTokenRepository(csrfTokenRepository());
-    }
+  
+  @Autowired
+  DataSource dataSource;
+  
+  @Autowired
+  UserDetailsService userDetailsService;
+  
+  @Bean
+  public DaoAuthenticationProvider authProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      http.authenticationProvider(authProvider())
+          .authorizeRequests()
+              .antMatchers("/", "/home", "/signup", "/resources/**").permitAll()
+              .anyRequest().authenticated()
+              .and()
+          .formLogin()
+              //.loginPage(LOGIN_PATH)
+              .usernameParameter("username").passwordParameter("password")
+              .permitAll()
+              .and()
+          .logout()
+              .permitAll()
+          .and().csrf()
+            .csrfTokenRepository(csrfTokenRepository());
+  }
+  
+  private CsrfTokenRepository csrfTokenRepository() 
+  { 
+      HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository(); 
+      repository.setSessionAttributeName("_csrf");
+      return repository; 
+  }
+  
+  @Bean(name="passwordEncoder")
+  public PasswordEncoder passwordEncoder(){
+   return new BCryptPasswordEncoder();
+  }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        /*auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-                */
-      auth.authenticationProvider(new AuthenticationProvider(){
-
-        @Override
-        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-          // TODO Auto-generated method stub
-          return null;
-        }
-
-        @Override
-        public boolean supports(Class<?> authentication) {
-          // TODO Auto-generated method stub
-          return false;
-        }});
-    }
-    
-    
-    private CsrfTokenRepository csrfTokenRepository() 
-    { 
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository(); 
-        repository.setSessionAttributeName("_csrf");
-        return repository; 
-    }
 }
